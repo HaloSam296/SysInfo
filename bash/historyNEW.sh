e316b28aba7f2ad623a9c8c1f3e2ab5b91330f6e
root <root@lapkali.grapplesam.com>
2024-04-25 16:22:44 -0500

    Continuing work; updating bash scripts and working on the new history script. More trouble than I thouht.

---
+++ bash/historyNEW.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This is the program that can be user modified
+#It's pretty simple, just gets the last x lines of history,
+#with x being defined by the user
+
+
+#Here's the super complicated and heavy bit of bash code:
+
+tail -n 6 ~/.bash_history
+
